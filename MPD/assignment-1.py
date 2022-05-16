be576019e88a1eb4998778a53dc8d115126e8da3
Dany Entezari <dany.entezari@gmail.com>
2022-05-16 19:24:22 +0400

    Added assignment 1.

---
+++ MPD/assignment-1.py
@@ -0,0 +1,64 @@
+# Python Assignment 1:
+# See steps 1 to 4 in this file.
+# DO NOT CHANGE ANY OF THE GIVEN LINES OF CODE. 
+productA = ["Apple iPhone 12", 4000]
+productB = ["Samsung Note 12", 4599]
+productC = ["Huwaei P40 Pro+", 3700]
+
+
+
+
+# (1) Use the appropriate List method(s) to add productA, productB, 
+# and productC to products_list. 
+# You must produce a list that looks like this...
+#
+# List methods: https://www.w3schools.com/python/python_ref_list.asp
+#
+# [
+#     ["Apple iPhone 12", 4000]
+#     ["Samsung Note 12", 4599]
+#     ["Huwaei P40 Pro+", 3700]  
+# ]
+products_list = []
+
+
+
+
+# (2) Use square brackets to add the keys and values of 
+# productA, productB, and productC to the products dictionary below. 
+# You must produce a dictionary that looks like this...
+# { 
+#       "Apple iPhone 12": 4000, 
+#       "Samsung Note 12": 4599, 
+#       "Huwaei P40 Pro+": 3700, 
+# }
+products_dict = {}
+
+
+
+
+# (3) Use dictionary methods to get the keys and values of 'products_dict'.
+# Use the sum() function to get the sum of the prices for all of the products.
+# Assign the total value to the variable totalPrice. 
+# totalPrice should become 12299
+#
+# Dictionary methods: https://www.w3schools.com/python/python_ref_dictionary.asp
+#
+total_price = 0;
+
+
+
+
+# (4) After populating products_list, use approproate control statement(s) to 
+# iterate over the products_list and to get the sum of the prices for all of 
+# the products. Hint: use the increment (+=) operator in your solution.
+# totalPrice should become 12299
+total_price = 0
+
+
+
+
+# (5) Use the print function and string methods to form this sentence,
+# "The price of Apple iPhone 12, Samsung Note 12, and Huwaei P40 Pro+ is 12299 AED"
+# Hint: See W3schools for reference
+
