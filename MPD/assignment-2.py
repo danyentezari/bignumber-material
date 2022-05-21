cf0ebcafe2ddf40e5b1e99b6dc9e09411c1ed078
Dany Entezari <dany.entezari@gmail.com>
2022-05-21 04:56:43 +0400

    Adding Assignment 2.

---
+++ MPD/assignment-2.py
@@ -0,0 +1,65 @@
+# Python Assignment 2:
+# See steps 1 and 2 in this file.
+# DO NOT CHANGE LINES 4 TO 20. 
+data = [
+    {
+        "id": "0002",
+        "company": "Netflakes",
+        "revenue": 1000000
+    },
+    {
+        "id": "0001",
+        "company": "Microsoap",
+        "revenue": 2000000
+    },
+    {
+        "id": "0003",
+        "company": "Burger Bing",
+        "revenue": 3000000,
+    }
+]
+
+
+
+
+# 1 - Swap the "id" of the first and second elements.
+#
+#     Note: do this without hardcoding the values.
+#     Hint: you can try using a temporary variable. 
+#
+#     Your result should produce:
+#     {
+#         "id": "0001",
+#         "company": "Netflakes",
+#         ...
+#     },
+#     {
+#         "id": "0002",
+#         "name": "Microsoap",
+#         ...
+#     },
+#    ...
+
+
+
+
+# 2 - Complete the function that will recursively 
+#     aggregate the values for every "revenue"
+#     in "data".  
+#
+#     Note: do not use either for or while loop.
+#     Hint: you can try adding other parameter(s)
+#     to the function.    
+#
+#     The result should be 6000000
+
+
+def recursive_sum(data):
+    
+    # Your code here...
+    
+    return #???
+
+
+# Test the function with print()
+# print(recursive_sum(data))
